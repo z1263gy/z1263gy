379f726bd007fd3c6ba7fe586377faef6b57e0af
patrick <patrick.corbett@finocomp.com>
2019-06-03 18:16:55 +1000

    Updates

---
+++ precache-manifest.b9b2a5c3ad73201aa96df88707be1ce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f11a71471234780dd364a3c5a953f49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29d270b4d560676708ac",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "29d270b4d560676708ac",
+    "url": "/static/js/main.b3cc1ec4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
